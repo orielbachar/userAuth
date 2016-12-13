d8cd04c77e196d9c6faf5c31fa5e9bfdd4f62c37
orielbachar <oriel.bachar@gmail.com>
2016-12-13 16:08:01 +0200

    missing logout

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+
+passport.serializeUser(function (user, done) {
+  console.log(user);
+  done(null, user.myID);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+
+passport.use('login', new LocalStrategy(function (username, password, done) {
+  var authenticated = username === "John" && password === "Smith";
+
+  if (authenticated) {
+    return done(null, { myUser:'user', myID: 1234 });
+  } else {
+    return done(null, false);
+  }
+}));
+
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/success',
+  failureRedirect: '/login',
+  // session: false
+}));
+
+app.get('/success', function (req, res){
+  if (!req.isAuthenticated()) {// Denied. Redirect to login
+   console.log('DEEEnied')
+   res.redirect('/login');
+
+//    app.get('/logout', function (req, res) {
+//   req.logout();
+//   res.send('Logged out!');
+// });
+
+ } else {
+   res.send('Hey, hello from the server!');
+ };
+
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.listen(8000);
